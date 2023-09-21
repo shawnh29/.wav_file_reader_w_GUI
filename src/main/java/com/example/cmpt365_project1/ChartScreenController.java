dee9a3d33d65891fa7cbc2d3364c16b93f234493
shawnh29 <Shawnhan20@gmail.com>
2023-09-21 00:11:37 -0700

    reading detailed parts of .wav file

--- src/main/java/com/example/cmpt365_project1/ChartScreenController.java
+++ src/main/java/com/example/cmpt365_project1/ChartScreenController.java
@@ -1,4 +1,69 @@
 package com.example.cmpt365_project1;
 
+import javafx.fxml.FXML;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.XYChart;
+
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.UnsupportedAudioFileException;
+import java.io.File;
+import java.io.IOException;
+
 public class ChartScreenController {
+    @FXML
+    private LineChart<?, ?> LineChart;
+    private File file;
+
+    public static ChartScreenController object;
+
+    public void setFile(File file) {
+        this.file = file;
+    }
+
+    public ChartScreenController() {
+
+    }
+
+    public static ChartScreenController getInstance() {
+        if (object == null) {
+            synchronized (ChartScreenController.class) {
+                if (object == null) {
+                    object = new ChartScreenController();
+                }
+            }
+        }
+        return object;
+    }
+
+    public void run() throws UnsupportedAudioFileException, IOException {
+
+        AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(file);
+        AudioFormat audioFormat = audioInputStream.getFormat();
+
+        float sampleRate = audioFormat.getSampleRate();
+        System.out.println("Sample Rate: " + sampleRate);
+        byte[] byteArray = audioInputStream.readAllBytes();
+
+        XYChart.Series series = new XYChart.Series();
+        series.getData().add(new XYChart.Data("1", 22));
+
+    }
+//    public void initialize() throws UnsupportedAudioFileException, IOException {
+//
+//        if (file != null) {
+//            AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(file);
+//            AudioFormat audioFormat = audioInputStream.getFormat();
+//
+//            float sampleRate = audioFormat.getSampleRate();
+//            System.out.println("Sample Rate: " + sampleRate);
+//            byte[] byteArray = audioInputStream.readAllBytes();
+//
+//            XYChart.Series series = new XYChart.Series();
+//            series.getData().add(new XYChart.Data("1", 22));
+//
+//        }
+//    }
+
 }
--- src/main/java/com/example/cmpt365_project1/MainController.java
+++ src/main/java/com/example/cmpt365_project1/MainController.java
@@ -11,14 +11,34 @@ import javafx.scene.image.Image;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
+import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.UnsupportedAudioFileException;
 import java.io.File;
 import java.io.IOException;
+import java.util.Objects;
 
 public class MainController {
     @FXML
     private Label welcomeText;
+
+    public static MainController object;
+
+    public MainController() {
+
+    }
+
+    public static MainController getInstance() {
+        if (object == null) {
+            synchronized (MainController.class) {
+                if (object == null) {
+                    object = new MainController();
+                }
+            }
+        }
+        return object;
+    }
 
     @FXML
     protected void onHelloButtonClick() {
@@ -26,7 +46,7 @@ public class MainController {
     }
 
     //function that opens a file explorer and prompts user to select a .wav file and processes it
-    public void openFileClicked(ActionEvent e) throws IOException {
+    public void openFileClicked(ActionEvent e) throws IOException, UnsupportedAudioFileException {
         System.out.println("Choose a file..");
         FileChooser fc = new FileChooser();
         fc.getExtensionFilters().add(new FileChooser.ExtensionFilter(".wav files", "*.wav"));
@@ -34,12 +54,18 @@ public class MainController {
 
         if (file != null) {
             System.out.println("File selected! --> " + file.getPath());
+
+            ChartScreenController.getInstance().setFile(file);
+            ChartScreenController.getInstance().run();
+
+            Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("ChartScreen.fxml")));
+            Stage stage = (Stage)((Node)e.getSource()).getScene().getWindow();
+            Scene scene = new Scene(root);
+            stage.setTitle("Waveform");
+            stage.setScene(scene);
+            stage.show();
+
         }
-        Parent root = FXMLLoader.load(getClass().getResource("ChartScreen.fxml"));
-        Stage stage = (Stage)((Node)e.getSource()).getScene().getWindow();
-        Scene scene = new Scene(root);
-        stage.setTitle("Waveform");
-        stage.setScene(scene);
-        stage.show();
+
     }
 }
